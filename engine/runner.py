a17f076707690c61202fc62fca3b381f39fcfe56
natasha-labs <zishazava@gmail.com>
2026-03-12 22:26:40 +0100

    Update runner.py

--- engine/runner.py
+++ engine/runner.py
@@ -1,9 +1,32 @@
-from tests.shadow.questions import questions
+from tests.registry import AVAILABLE_TESTS
+import importlib
+
+
+def choose_test():
+
+    print("Доступные тесты:\n")
+
+    tests = list(AVAILABLE_TESTS.keys())
+
+    for i, key in enumerate(tests, start=1):
+        print(f"{i}. {AVAILABLE_TESTS[key]['title']}")
+
+    choice = int(input("\nВыберите тест: "))
+
+    return tests[choice - 1]
 
 
 def run_test():
 
-    print("Shadow Test\n")
+    test_key = choose_test()
+
+    module_name = AVAILABLE_TESTS[test_key]["module"]
+
+    module = importlib.import_module(module_name)
+
+    questions = module.questions
+
+    print("\nНачинаем тест\n")
 
     answers = []
 
