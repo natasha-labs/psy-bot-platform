c9033ba94725187a864bf546692a3f514a0daee4
natasha-labs <zishazava@gmail.com>
2026-03-12 23:29:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from engine.runner import run_test
 
-if __name__ == "__main__":
-    run_test()
+TOKEN = "8691203465:AAEpm9I_xLJQXORp7OQJwiPdEI4UNn2AXWU"
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    await update.message.reply_text(
+        "Добро пожаловать в систему 'Код личности'\n\n"
+        "Скоро здесь появятся психологические тесты."
+    )
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
