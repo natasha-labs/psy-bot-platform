3b0864dad7147bb39ed2e955148a99c04a9e2c64
natasha-labs <zishazava@gmail.com>
2026-03-16 19:32:46 +0100

    Update registry.py

---
+++ personality_code/registry.py
@@ -0,0 +1,20 @@
+FREE_TESTS = [
+    "archetype",
+    "shadow",
+    "anxiety",
+]
+
+PREMIUM_TESTS = [
+    "love_code",
+    "attachment_style",
+    "inner_conflict",
+    "defense_mechanisms",
+]
+
+TEST_ROLES = {
+    "archetype": "identity_core",
+    "shadow": "shadow_layer",
+    "anxiety": "stress_modifier",
+}
+
+BASIC_REQUIRED_TESTS = FREE_TESTS[:]
