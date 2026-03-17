6d3a7d794721ae68fcced216f735ce85d96b6e17
natasha-labs <zishazava@gmail.com>
2026-03-18 00:15:21 +0100

    Update registry.py

--- tests/registry.py
+++ tests/registry.py
@@ -1,9 +1,9 @@
-from tests.shadow import TEST as SHADOW
-from tests.archetype import TEST as ARCHETYPE
-from tests.anxiety import TEST as ANXIETY
+from tests.shadow.test_def import TEST_DEF as SHADOW_TEST
+from tests.archetype.test_def import TEST_DEF as ARCHETYPE_TEST
+from tests.anxiety.test_def import TEST_DEF as ANXIETY_TEST
 
 TESTS = {
-    "shadow": SHADOW,
-    "archetype": ARCHETYPE,
-    "anxiety": ANXIETY,
+    "shadow": SHADOW_TEST,
+    "archetype": ARCHETYPE_TEST,
+    "anxiety": ANXIETY_TEST,
 }
