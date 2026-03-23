5cf0d4026a92d7833b592cdf27a3f1157669c797
natasha-labs <zishazava@gmail.com>
2026-03-23 14:33:13 +0100

    Update registry.py

--- tests/registry.py
+++ tests/registry.py
@@ -1,5 +1,11 @@
-from tests.deep_profile.test_def import TEST_DEF as DEEP
+from tests.anxiety.test_def import TEST_DEF as ANXIETY_TEST
+from tests.archetype.test_def import TEST_DEF as ARCHETYPE_TEST
+from tests.shadow.test_def import TEST_DEF as SHADOW_TEST
+from tests.deep_profile.test_def import TEST_DEF as DEEP_PROFILE_TEST
 
 TESTS = {
-    "deep_profile": DEEP
+    "anxiety": ANXIETY_TEST,
+    "archetype": ARCHETYPE_TEST,
+    "shadow": SHADOW_TEST,
+    "deep_profile": DEEP_PROFILE_TEST,
 }
