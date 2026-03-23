056f295d6fc6b7779e8deb69696109d9684663c8
natasha-labs <zishazava@gmail.com>
2026-03-23 14:53:53 +0100

    Update questions.py

--- tests/deep_profile/questions.py
+++ tests/deep_profile/questions.py
@@ -53,4 +53,70 @@ questions = [
         "id": 6,
         "text": "Если не по плану:",
         "options": [
-            {"text": "контроль", "value": {"control": 1
+            {"text": "контроль", "value": {"control": 1}},
+            {"text": "переживание", "value": {"analyze": 1}},
+            {"text": "замираешь", "value": {"avoid": 1}},
+            {"text": "ищешь выход", "value": {"react": 1}},
+        ],
+    },
+    {
+        "id": 7,
+        "text": "Разговор:",
+        "options": [
+            {"text": "прямо", "value": {"react": 1}},
+            {"text": "недоговариваешь", "value": {"control": 1}},
+            {"text": "уходишь", "value": {"avoid": 1}},
+            {"text": "говоришь одно, чувствуешь другое", "value": {"analyze": 1}},
+        ],
+    },
+    {
+        "id": 8,
+        "text": "Риск:",
+        "options": [
+            {"text": "действуешь", "value": {"react": 1}},
+            {"text": "тормозишь", "value": {"avoid": 1}},
+            {"text": "откладываешь", "value": {"avoid": 1}},
+            {"text": "ищешь идеальный вариант", "value": {"analyze": 1}},
+        ],
+    },
+    {
+        "id": 9,
+        "text": "Напряжение:",
+        "options": [
+            {"text": "выплеск", "value": {"react": 1}},
+            {"text": "держишь", "value": {"control": 1}},
+            {"text": "отвлекаешься", "value": {"avoid": 1}},
+            {"text": "анализируешь", "value": {"analyze": 1}},
+        ],
+    },
+    {
+        "id": 10,
+        "text": "Проявление:",
+        "options": [
+            {"text": "идёшь", "value": {"react": 1}},
+            {"text": "сомневаешься", "value": {"analyze": 1}},
+            {"text": "откладываешь", "value": {"avoid": 1}},
+            {"text": "ждёшь", "value": {"analyze": 1}},
+        ],
+    },
+    {
+        "id": 11,
+        "text": "Сложность:",
+        "options": [
+            {"text": "берёшь ответственность", "value": {"control": 1}},
+            {"text": "перегружаешься мыслями", "value": {"analyze": 1}},
+            {"text": "уходишь в себя", "value": {"avoid": 1}},
+            {"text": "пытаешься не думать", "value": {"avoid": 1}},
+        ],
+    },
+    {
+        "id": 12,
+        "text": "Когда становится напряжённо:",
+        "options": [
+            {"text": "начинаешь контролировать", "value": {"control": 1}},
+            {"text": "уходишь внутрь себя", "value": {"avoid": 1}},
+            {"text": "реагируешь резко", "value": {"react": 1}},
+            {"text": "зависаешь", "value": {"analyze": 1}},
+        ],
+    },
+]
